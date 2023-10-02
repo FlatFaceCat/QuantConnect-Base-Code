59cbcd63ceea881fa22c95d0d0c96aa001ebc5d1
FlatFaceCat <80037958+FlatFaceCat@users.noreply.github.com>
2023-10-02 16:00:23 +0100

    basecode-10022023

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Base code for dealing with futures and specific candle sizes on Quant connect
 ### How to:
 Please follow steps here: https://github.com/FlatFaceCat/QuantConnect-Local-Setup-Guide to follow steps on working with Quant connect and Lean. 
 
-- Clone this repo to a local folder
-- 
+### Notes
+- This is base code that works with Future contracts (in this case ES) and takes into count the contract expiring and roll overs 
+- This base code has pre set consolidators that trigger at one, five, fifteen and thirty minute intervals. (OHLC for each candle range is defined)
+- This is purely base code that has no triggers and will just cycle through all available data feeds. 
--- main.py
+++ main.py
@@ -1,21 +1,120 @@
 # region imports
 from AlgorithmImports import *
+import pandas as pd
+import numpy as np
+from datetime import datetime, date, timedelta, time
+import collections
+from order_codes import (OrderTypeCodes, OrderDirectionCodes, OrderStatusCodes)
+from decimal import *
 # endregion
 
-class Test(QCAlgorithm):
+
+class Trialcode(QCAlgorithm):
 
     def Initialize(self):
-        # Locally Lean installs free sample data, to download more data please visit https://www.quantconnect.com/docs/v2/lean-cli/datasets/downloading-data
-        self.SetStartDate(2013, 10, 7)  # Set Start Date
-        self.SetEndDate(2013, 10, 11)  # Set End Date
+        self.SetStartDate(2020, 10, 6)
+        self.SetEndDate(2023, 10, 11)
         self.SetCash(100000)  # Set Strategy Cash
-        self.AddEquity("SPY", Resolution.Minute)
 
-    def OnData(self, data: Slice):
-        """OnData event is the primary entry point for your algorithm. Each new data point will be pumped in here.
-            Arguments:
-                data: Slice object keyed by symbol containing the stock data
-        """
-        if not self.Portfolio.Invested:
-            self.SetHoldings("SPY", 1)
-            self.Debug("Purchased Stock")
+        self.new_day = True
+        self.contract = None
+
+        # Subscribe and set our expiry filter for the futures chain
+        # Find more symbols here: http://quantconnect.com/data
+        futureES = self.AddFuture(Futures.Indices.SP500EMini, Resolution.Minute)
+        futureES.SetFilter(TimeSpan.Zero, TimeSpan.FromDays(185))         
+
+    def OnData(self, slice):
+        self.InitUpdateContract(slice)
+
+    def InitUpdateContract(self, slice):
+        # Reset daily - everyday we check whether futures need to be rolled
+        if not self.new_day:
+            return
+        # rolling 3 days before expiry
+        if self.contract != None and (self.contract.Expiry - self.Time).days >= 3:
+            return
+
+        for chain in slice.FutureChains.Values:
+            # If we trading a contract, send to log how many days until the contract's expiry
+            if self.contract != None:
+                self.Log('Expiry days away {} - {}'.format((self.contract.Expiry -
+                         self.Time).days, self.contract.Expiry))
+
+                # Reset any open positions based on a contract rollover.
+                self.Log('RESET: closing all positions')
+                self.Liquidate()
+
+            # get list of contracts
+            contracts = list(chain.Contracts.Values)
+            # [contract for contract in chain]
+            chain_contracts = list(contracts)
+            # order list of contracts by expiry date: newest --> oldest
+            chain_contracts = sorted(chain_contracts, key=lambda x: x.Expiry)
+
+            # pick out contract and log contract name
+            self.contract = chain_contracts[1]
+            self.Log("Setting contract to: {}".format(
+                self.contract.Symbol.Value))
+
+            # Set up consolidators.
+            one_min = TradeBarConsolidator(TimeSpan.FromMinutes(1))
+            one_min.DataConsolidated += self.OnOneMin
+            self.SubscriptionManager.AddConsolidator(
+                self.contract.Symbol, one_min)
+            five_min = TradeBarConsolidator(TimeSpan.FromMinutes(5))
+            five_min.DataConsolidated += self.OnFiveMin
+            self.SubscriptionManager.AddConsolidator(
+                self.contract.Symbol, five_min)
+            fifteen_min = TradeBarConsolidator(TimeSpan.FromMinutes(15))
+            fifteen_min.DataConsolidated += self.OnFifteenMin
+            self.SubscriptionManager.AddConsolidator(
+                self.contract.Symbol, fifteen_min)
+            thirty_min = TradeBarConsolidator(TimeSpan.FromMinutes(30))
+            thirty_min.DataConsolidated += self.OnThirtyMin
+            self.SubscriptionManager.AddConsolidator(
+                self.contract.Symbol, thirty_min)
+
+            # reset the new day flag
+            self.new_day = False
+            self.Liquidate()
+
+    def OnOneMin(self, sender, bar):
+        if bar.Symbol == self.contract.Symbol:
+            openPrice = bar.Open
+            highPrice = bar.High
+            lowPrice = bar.Low
+            closePrice = bar.Close
+        pass
+
+    def OnFiveMin(self, sender, bar):
+        if bar.Symbol == self.contract.Symbol:
+            openPrice = bar.Open
+            highPrice = bar.High
+            lowPrice = bar.Low
+            closePrice = bar.Close
+        pass
+
+    def OnFifteenMin(self, sender, bar):
+        if bar.Symbol == self.contract.Symbol:
+            openPrice = bar.Open
+            highPrice = bar.High
+            lowPrice = bar.Low
+            closePrice = bar.Close
+        pass
+
+    def OnThirtyMin(self, sender, bar):
+        if bar.Symbol == self.contract.Symbol:
+            openPrice = bar.Open
+            highPrice = bar.High
+            lowPrice = bar.Low
+            closePrice = bar.Close
+        pass
+
+    def OnEndOfDay(self):
+        self.new_day = True
+
+    def OnOrderEvent(self, orderEvent):
+        if orderEvent.Status != OrderStatus.Filled:
+            return
+
